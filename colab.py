b9c6108c9c6c5b437bbe3f9fa30236d855947f90
amlmoawad <https://amlmoawad174@gmail.com/>
2024-01-26 23:43:41 +0200

    Update colab.py

--- colab.py
+++ colab.py
@@ -30,30 +30,19 @@ class ImageClassificationBase(nn.Module):
         print("Epoch [{}], last_lr: {:.5f}, train_loss: {:.4f}, val_loss: {:.4f}, val_acc: {:.4f}".format(
             epoch, result['lrs'][-1], result['train_loss'], result['val_loss'], result['val_accuracy']))
         
-    def __init__(self, in_channels, num_diseases):
+class SimpleResidualBlock(nn.Module):
+    def __init__(self):
         super().__init__()
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=3, kernel_size=3, stride=1, padding=1)
+        self.relu1 = nn.ReLU()
+        self.conv2 = nn.Conv2d(in_channels=3, out_channels=3, kernel_size=3, stride=1, padding=1)
+        self.relu2 = nn.ReLU()
         
-        self.conv1 = ConvBlock(in_channels, 64)
-        self.conv2 = ConvBlock(64, 128, pool=True) # out_dim : 128 x 64 x 64 
-        self.res1 = nn.Sequential(ConvBlock(128, 128), ConvBlock(128, 128))
-        
-        self.conv3 = ConvBlock(128, 256, pool=True) # out_dim : 256 x 16 x 16
-        self.conv4 = ConvBlock(256, 512, pool=True) # out_dim : 512 x 4 x 44
-        self.res2 = nn.Sequential(ConvBlock(512, 512), ConvBlock(512, 512))
-        
-        self.classifier = nn.Sequential(nn.MaxPool2d(4),
-                                       nn.Flatten(),
-                                       nn.Linear(512, num_diseases))
-        
-    def forward(self, xb): # xb is the loaded batch
-        out = self.conv1(xb)
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.relu1(out)
         out = self.conv2(out)
-        out = self.res1(out) + out
-        out = self.conv3(out)
-        out = self.conv4(out)
-        out = self.res2(out) + out
-        out = self.classifier(out)
-        return out               
+        return self.relu2(out) + x # ReLU can be applied before or after adding the input              
 # Architecture for training
 
 # convolution block with BatchNormalization
